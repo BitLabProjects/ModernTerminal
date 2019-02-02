244081e98c063313f8d1aece656ff5c7930f305f
xed89 <simone.bosc@gmail.com>
2019-02-02 11:49:26 +0100

    Fixed animated underline animation

---
+++ Controls/Underline.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernTerminal
+{
+  class Underline: Control
+  {
+    public Underline() {
+      DefaultStyleKey = typeof(Underline);
+    }
+
+    public override void OnApplyTemplate() {
+      base.OnApplyTemplate();
+      UpdateVisualState();
+    }
+
+    private void UpdateVisualState() {
+      var newState = IsActive ? "Active" : "Inactive";
+      VisualStateManager.GoToState(this, newState, true);
+    }
+
+    public bool IsActive {
+      get { return (bool)GetValue(IsActiveProperty); }
+      set { SetValue(IsActiveProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsActiveProperty =
+        DependencyProperty.Register("IsActive", typeof(bool), typeof(Underline), new PropertyMetadata(false, IsActive_PropertyChanged));
+
+    private static void IsActive_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+      var u = (Underline)d;
+      u.UpdateVisualState();
+    }
+  }
+}
